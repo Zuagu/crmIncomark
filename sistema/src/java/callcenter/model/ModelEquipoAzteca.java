123f405f423d067b01801763cdaffc0ec6ea5dc7
zuagu_medina <cruguer96@gmail.com>
2020-10-19 20:16:19 -0500

    web xml para detectar

--- sistema/src/java/callcenter/model/ModelEquipoAzteca.java
+++ sistema/src/java/callcenter/model/ModelEquipoAzteca.java
@@ -141,7 +141,7 @@ public class ModelEquipoAzteca {
     public static String select_gerente_options(String territorio) {
         try {
             StartConexion inicioConexion = new StartConexion();
-            String sql = "SELECT GERENTE FROM azteca_base_genenral_original WHERE TERRITORIO IN("+ territorio.replace("\"", "'") +") GROUP BY GERENTE;";
+            String sql = "SELECT GERENTE FROM azteca_base_genenral_original WHERE TERRITORIO IN(" + territorio.replace("\"", "'") + ") GROUP BY GERENTE;";
             System.out.println(sql);
             inicioConexion.rs = inicioConexion.st.executeQuery(sql);
 
@@ -163,7 +163,7 @@ public class ModelEquipoAzteca {
     public static String select_etapas_options(String territorio, String gerente) {
         try {
             StartConexion inicioConexion = new StartConexion();
-            String sql = "SELECT ETAPA FROM azteca_base_genenral_original WHERE TERRITORIO IN ("+territorio+") AND GERENTE IN ("+gerente+") GROUP BY ETAPA;";
+            String sql = "SELECT ETAPA FROM azteca_base_genenral_original WHERE TERRITORIO IN (" + territorio + ") AND GERENTE IN (" + gerente + ") GROUP BY ETAPA;";
             System.out.println(sql);
             inicioConexion.rs = inicioConexion.st.executeQuery(sql);
 
@@ -207,7 +207,7 @@ public class ModelEquipoAzteca {
             StartConexion inicioConexion = new StartConexion();
             String sql_act_data_equipo = "CALL azteca_actualizar_datos_equipos();";
             String sql_ob_equi_max = "SELECT ID_EQUIPO FROM azteca_equipos order by ID_EQUIPO desc limit 1;";
-            int id_max = 0; 
+            int id_max = 0;
             System.out.println(sql_ob_equi_max);
             inicioConexion.rs = inicioConexion.st.executeQuery(sql_ob_equi_max);
 
@@ -216,19 +216,19 @@ public class ModelEquipoAzteca {
             while (inicioConexion.rs.next()) {
                 id_max = inicioConexion.rs.getInt("ID_EQUIPO");
             }
-            
+
             System.out.println(id_max);
-            
+
             String sql = "UPDATE azteca_base_genenral_original SET \n"
-                    + "ID_EQUIPO = "+ id_max +"\n"
+                    + "ID_EQUIPO = " + id_max + "\n"
                     + "WHERE TERRITORIO IN (" + territorios.replace("\"", "'") + ") \n"
                     + "	AND GERENTE IN (" + gerentes.replace("\"", "'") + ") \n"
                     + "    AND ETAPA IN (" + etapa.replace("\"", "'") + ");";
             System.out.println(sql);
             inicioConexion.st.executeUpdate(sql);
-            
+
             inicioConexion.rs = inicioConexion.st.executeQuery(sql_act_data_equipo);
-            
+
             inicioConexion.rs.close();
             inicioConexion.st.close();
             inicioConexion.conn.close();
@@ -238,33 +238,142 @@ public class ModelEquipoAzteca {
             return "sql code" + e;
         }
     }
-    
+
     public static String agregar_nuevas_cuentas_equipo(String territorios, String gerentes, String etapa, String id_equipo) {
         try {
             StartConexion inicioConexion = new StartConexion();
-            
+
             String sql_act_data_equipo = "CALL azteca_actualizar_datos_equipos();";
             System.out.println(sql_act_data_equipo);
-            
+
             JSONObject response = new JSONObject();
-            
+
             String sql = "UPDATE azteca_base_genenral_original SET \n"
-                    + "ID_EQUIPO = "+ id_equipo +"\n"
+                    + "ID_EQUIPO = " + id_equipo + "\n"
                     + "WHERE TERRITORIO IN (" + territorios.replace("\"", "'") + ") \n"
                     + "	AND GERENTE IN (" + gerentes.replace("\"", "'") + ") \n"
                     + "    AND ETAPA IN (" + etapa.replace("\"", "'") + ");";
             System.out.println(sql);
             inicioConexion.st.executeUpdate(sql);
-            
+
             inicioConexion.rs = inicioConexion.st.executeQuery(sql_act_data_equipo);
-            
+
             response.put("response", "ok");
-            
+
             inicioConexion.rs.close();
             inicioConexion.st.close();
             inicioConexion.conn.close();
 
             return response.toString();
+        } catch (SQLException e) {
+            return "sql code" + e;
+        }
+    }
+
+    public static String select_asignaiones_usuarios() {
+        try {
+            StartConexion inicioConexion = new StartConexion();
+
+            String sql = "select u.id as id_gestor, \n"
+                    + "	count(b.CLIENTE_UNICO) as cantidad, \n"
+                    + "    format(sum(b.SALDO_TOTAL),2) as importe,\n"
+                    + "    ifnull(nombre_usuario_alias(b.ID_GESTOR_ASIGNADO), 'No Asignado') gestor,\n"
+                    + "    ifnull(u.orden,'ninguno') as orden, \n"
+                    + "    ifnull(u.orden_importe,'ninguno') as orden_importe \n"
+                    + "from azteca_base_genenral_original b\n"
+                    + "left join arcade_usuarios u on u.id = b.ID_GESTOR_ASIGNADO \n"
+                    + "where IDENTIFICADOR != '0' group by b.ID_GESTOR_ASIGNADO;";
+            JSONArray asig_gestores = new JSONArray();
+
+            inicioConexion.rs = inicioConexion.st.executeQuery(sql);
+
+            while (inicioConexion.rs.next()) {
+                JSONObject gestor = new JSONObject();
+                gestor.put("id_gestor", inicioConexion.rs.getString("id_gestor"));
+                gestor.put("cantidad", inicioConexion.rs.getString("cantidad"));
+                gestor.put("importe", inicioConexion.rs.getString("importe"));
+                gestor.put("gestor", inicioConexion.rs.getString("gestor"));
+                gestor.put("orden", inicioConexion.rs.getString("orden"));
+                gestor.put("orden_importe", inicioConexion.rs.getString("orden_importe"));
+                asig_gestores.add(gestor);
+            }
+
+            inicioConexion.rs.close();
+            inicioConexion.st.close();
+            inicioConexion.conn.close();
+
+            return asig_gestores.toString();
+        } catch (SQLException e) {
+            return "sql code" + e;
+        }
+    }
+
+    public static String update_config_gestor(String val_order, String val_order_importe, String id_gestor) {
+        try {
+            StartConexion inicioConexion = new StartConexion();
+
+            String sql = "update arcade_usuarios set orden = '" + val_order + "', orden_importe = '" + val_order_importe + "' where id = '" + id_gestor + "';";
+//            JSONArray asig_gestores = new JSONArray();
+            JSONObject response = new JSONObject();
+
+            inicioConexion.st.executeUpdate(sql);
+            response.put("response", "Actualizado con exito");
+
+//            inicioConexion.rs.close();
+            inicioConexion.st.close();
+            inicioConexion.conn.close();
+
+            return response.toString();
+        } catch (SQLException e) {
+            return "sql code" + e;
+        }
+    }
+    
+    
+    public static String update_config_todos_gestor(String order_ult_gestion, String order_monto) {
+        try {
+            StartConexion inicioConexion = new StartConexion();
+
+            String sql = "update arcade_usuarios set orden = '" + order_ult_gestion + "', orden_importe = '" + order_monto + "';";
+//            JSONArray asig_gestores = new JSONArray();
+            JSONObject response = new JSONObject();
+
+            inicioConexion.st.executeUpdate(sql);
+            response.put("response", "Actualizado con exito");
+
+//            inicioConexion.rs.close();
+            inicioConexion.st.close();
+            inicioConexion.conn.close();
+
+            return response.toString();
+        } catch (SQLException e) {
+            return "sql code" + e;
+        }
+    }
+
+    public static String estatus_asignacion_gestor(String id_gestor) {
+        try {
+            StartConexion inicioConexion = new StartConexion();
+
+            String sql = "select count(CLIENTE_UNICO) as cantidad,\n"
+                    + "	nombre_estatus_llamada_azteca(ID_ESTATUS_LLAMADA) as ESTATUS_LLAMADA\n"
+                    + "from azteca_base_genenral_original where ID_GESTOR_ASIGNADO = '"+id_gestor+"' and IDENTIFICADOR != '0' group by ID_ESTATUS_LLAMADA;";
+            JSONArray est_gestores = new JSONArray();
+
+            inicioConexion.rs = inicioConexion.st.executeQuery(sql);
+
+            while (inicioConexion.rs.next()) {
+                JSONObject gestor = new JSONObject();
+                gestor.put("ESTATUS_LLAMADA", inicioConexion.rs.getString("ESTATUS_LLAMADA"));
+                gestor.put("cantidad", inicioConexion.rs.getString("cantidad"));
+                est_gestores.add(gestor);
+            }
+
+            inicioConexion.rs.close();
+            inicioConexion.st.close();
+            inicioConexion.conn.close();
+
+            return est_gestores.toString();
         } catch (SQLException e) {
             return "sql code" + e;
         }
